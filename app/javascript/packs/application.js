abe9df02445f322a56a295d62a2aafb4e7bd9ec2
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-03-03 11:15:06 +0100

    Install bootstrap correctly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
