8095b21b68db7c24fe8014519ea94c86527e835f
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-03-04 10:24:55 +0100

    Add attendee_id column to schema file

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,7 @@
 class Invite < ApplicationRecord
     belongs_to :attendee, class_name: "User"
     belongs_to :attended_event, class_name: "Event"
+    belongs_to :attendee_id, class_name: "User"
   
     validates :attendee_id, presence: true
     validates :attended_event_id, presence: true
