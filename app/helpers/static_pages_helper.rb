7929c2bbb0086827951f1da9b7565db876f421d3
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-03-16 21:27:44 +0100

    Fix rubocop errors

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,17 +1,17 @@
 module StaticPagesHelper
-    def header_link
-        if signed_in?
-            return {
-                text: "#{current_user.name}(Sign Out)",
-                path: signout_path,
-                method: "delete"
-            }
-        else
-            return {
-                text: "Sign In",
-                path: signin_path,
-                method: nil
-            }
-        end
+  def header_link
+    if signed_in?
+      {
+        text: "#{current_user.name}(Sign Out)",
+        path: signout_path,
+        method: 'delete'
+      }
+    else
+      {
+        text: 'Sign In',
+        path: signin_path,
+        method: nil
+      }
     end
+  end
 end
